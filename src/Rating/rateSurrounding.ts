03fc69e44b2441956c4c1bacc7c7446b22f14407
Henrik <henrik@thoroee.com>
2019-12-22 21:33:02 +0100

    Bug Fix

--- src/Rating/rateSurrounding.ts
+++ src/Rating/rateSurrounding.ts
@@ -1,9 +1,9 @@
 import { State, Move, getNeighbours } from "@henrikthoroe/swc-client";
-import { foreach, flat, filter } from "@henrikthoroe/swc-client/dist/utils";
+import { foreach, flat, filter, map } from "@henrikthoroe/swc-client/dist/utils";
 import { Type } from "@henrikthoroe/swc-client/dist/client/Model/Piece";
 
 export default function rateSurrounding(state: State, moves: Move[]): number {
-    const allFields = flat(state.board.fields)
+    const allFields = flat(map(state.board.fields, group => filter(group, field => field !== null)))
 
     for (const field of allFields) {
         if (field.pieces.findIndex(piece => piece.owner !== state.currentPlayer && piece.type === Type.BEE) !== -1) {
