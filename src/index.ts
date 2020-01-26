b6187ef96c8969010ab3290f5b2a60b1cc321dcd
Henrik Thoroe <henrik@thoroee.com>
2020-01-26 13:14:21 +0100

    Some random

--- src/index.ts
+++ src/index.ts
@@ -29,159 +29,30 @@ connect({ host: args.host || "localhost", port: args.port || 13050, joinOptions:
         throw new Error("No available moves") // send missmove
     }
 
-    if (state.undeployed.red.findIndex(p => p.type === Type.BEE && p.owner === player.color) !== -1) {
-        available = available.filter(move => {
-            if ((move.start as Piece).type) {
-                return (move.start as Piece).type === Type.BEE
-            }
+    let selected: Move | null = null
+    let max = -Infinity
+    const currentRating = rate(state, player.color)
 
-            return false
-        })
+    for (const move of available) {
+        const next = nextState(state, move)
+        const rating = rate(next, player.color)
 
+        if (rating > max) {
+            max = rating
+            selected = move
+        }
+
+        if (rating === max && Math.random() > 0.8) {
+            selected = move
+        }
+    }
+
+    if (currentRating >= max) {
         return available[Math.floor(Math.random() * available.length)]
     }
 
-    let selectedMove: Move | null = null
-    const horizon = 5
-    const time = () => Date.now()
-    const start = time()
-    const timeout = 1990
-
-    const name = (color: Color) => {
-        return color === Color.Red ? "Red" : "Blue"
-    }
-
-    const max = (depth: number, state: State, moves: Move[], alpha: number, beta: number) => {
-        if (depth === 0 || moves.length === 0) {
-            // console.log(`Rating max ${name(state.currentPlayer)}`)
-            return rate(state, player.color)
-        }
-
-        let max = alpha
-
-        for (const move of moves) {
-
-            if (time() - start > timeout) {
-                break
-            }
-            
-            const next = nextState(state, move)
-            const value = min(depth - 1, next, fetchMoves(state), max, beta)
-
-            if (value > max) {
-                max = value
-
-                if (depth === horizon) {
-                    selectedMove = move
-                }
-
-                if (max >= beta) {
-                    break
-                }
-            }
-        }
-
-        return max
-    }
-
-    const min = (depth: number, state: State, moves: Move[], alpha: number, beta: number) => {
-        if (depth === 0 || moves.length === 0) {
-            // console.log(`Rating min ${name(state.currentPlayer)}`)
-            return rate(state, player.color)
-        }
-
-        let min = beta
-
-        for (const move of moves) {
-
-            if (time() - start > timeout) {
-                break
-            }
-
-            const next = nextState(state, move)
-            const value = max(depth - 1, next, fetchMoves(state), alpha, min)
-
-            if (value < min) {
-                min = value
-
-                if (depth === horizon) {
-                    selectedMove = move
-                }
-
-                if (min <= alpha) {
-                    break
-                }
-            }
-        }
-
-        return min
-    }
-
-    const alphaBeta = (depth: number, state: State, moves: Move[], alpha: number, beta: number): number => {
-        if (depth === 0 || moves.length === 0) {
-            return rate(state, player.color)
-        }
-
-        let maxValue = alpha
-
-
-        for (let i = 0; i < moves.length; ++i) {
-            const next = nextState(state, moves[i])
-            const nextMoves = fetchMoves(next)
-            const value = -alphaBeta(depth - 1, next, nextMoves, -beta, -maxValue)
-
-            if (value > maxValue) {
-                maxValue = value
-                if (depth === horizon) {
-                    selectedMove = moves[i]
-                }
-                if (maxValue >= beta) {
-                    break
-                }
-            }
-        }
-
-        return maxValue
-    }
-
-    console.time()
-    // console.log(alphaBeta(horizon, state, available, -Infinity, Infinity))
-    console.log(max(horizon, state, available, -Infinity, Infinity))
-    console.timeEnd()
-
-    return selectedMove || available[Math.floor(Math.random() * available.length)]
+    return selected || available[Math.floor(Math.random() * available.length)]
 })
 .catch(error => {
     console.error(error)
 })
-
-// function openingMove(moves: Move[]): Move {
-//     let min = moves[0]
-
-//     for (const move of moves) {
-        
-//     }
-// }
-
-
-// function minmax(state: State, depth: number,  selectedMove: { move?: Move }): number {
-//     const moves = fetchMoves(state)
-//     let max = -Infinity
-
-//     if (moves.length === 0 || depth === 0) {
-//         return conclude(rate(state, moves))
-//     }
-
-//     while (moves.length > 0) {
-//         const move =  moves.pop()!
-//         const next  = nextState(state, move)
-//         const rating = -minmax(next, depth - 1, selectedMove)
-
-//         if (rating > max) {
-//             max = rating
-//             selectedMove.move = move
-//         }
-//     }
-
-//     return max
-// }
