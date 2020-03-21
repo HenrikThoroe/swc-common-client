81e27ee81b4cdfb80cfbe79b95678d7bc057217f
Henrik Thoroe <henrik@thoroee.com>
2020-03-21 12:09:30 +0100

    Custom Base64 Encoding

--- src/utils/encodeBase64.ts
+++ src/utils/encodeBase64.ts
@@ -1,19 +1,19 @@
 const base64Alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/".split("")
 
 export default function encodeBase64(x: number): string {
-    // let result = ""
-    // const blockLength = 6
-    // const bitsToShift = 32 - blockLength
+    let result = ""
+    const blockLength = 6
+    const bitsToShift = 32 - blockLength
 
-    // while (x > 0) {
-    //     const y = x << bitsToShift >>> bitsToShift  // Read block
-    //     x = x >>> blockLength                       // Remove block from number
-    //     result += base64Alphabet[y]                 // look up the associated character for block
-    // }
+    while (x > 0) {
+        const y = x << bitsToShift >>> bitsToShift  // Read block
+        x = x >>> blockLength                       // Remove block from number
+        result += base64Alphabet[y]                 // look up the associated character for block
+    }
 
-    // return result
+    return result
 
-    return Buffer.alloc(8, x).toString("base64").replace("==", "")
+    // return Buffer.alloc(8, x).toString("base64").replace("==", "")
 
     // let result = ""
     // let temp = 0
