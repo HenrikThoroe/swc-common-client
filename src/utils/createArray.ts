e7312fd5ac10b54df3a6018db69bd4d55c3edaba
Henrik Thoroe <henrik@thoroee.com>
2020-03-21 20:19:48 +0100

    Delete Unused Files

--- src/utils/createArray.ts
+++
@@ -1,3 +0,0 @@
-export default function createArray<T>(len: number, content: T): T[] {
-    return Array.from(Array<number>(len)).map(() => content)
-}
--- src/utils/createCubeArray.ts
+++
@@ -1,5 +0,0 @@
-import createArray from "./createArray";
-
-export default function createCubeArray<T>(len: number, content: T): T[][][] {
-    return createArray(len, createArray(len, createArray(len, content)))
-}
