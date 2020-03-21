682479aee4ea56f2addefede9fd226d3318063bf
Henrik Thoroe <henrik@thoroee.com>
2020-03-21 13:09:32 +0100

    Move Cache

--- src/LookAhead/generateMoves.ts
+++ src/LookAhead/generateMoves.ts
@@ -1,5 +1,18 @@
 import { fetchMoves, State, Move } from "@henrikthoroe/swc-client"
+import LookupTable from "../Cache/LookupTable"
+import hashState from "../utils/hashState"
+
+const moveTable = new LookupTable<State, Move[]>(3000, hashState)
 
 export default function generateMoves(state: State): Move[] {
-    return fetchMoves(state, false)
+    const cached = moveTable.read(state)
+
+    if (cached) {
+        return cached
+    }
+
+    const moves = fetchMoves(state)
+    moveTable.push(state, moves)
+
+    return moves
 }
