d4a61ed02e31b3a700fec41a2b615b3458b17921
Henrik Thoroe <henrik@thoroee.com>
2020-03-22 20:03:54 +0100

    Clean Code

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,8 @@
 import connect, { Move, State, Result, Player, ConnectOptions } from '@henrikthoroe/swc-client'
-import rate from './Rating/rate'
 import yargs from 'yargs'
 import Piece from '@henrikthoroe/swc-client/dist/client/Model/Piece'
 import handleSpecialCase from './Logic/handleSpecialCase'
 import Timer from './utils/Timer'
-import simulateMove from './LookAhead/simulateMove'
 import NegaScout from './Logic/NagaScout'
 import sortMoves from './Logic/sortMoves'
 
@@ -23,10 +21,6 @@ const args = yargs
     })
     .option("stupid", {
         alias: "s",
-        type: 'boolean'
-    })
-    .option("ai", {
-        alias: "a",
         type: 'boolean'
     })
     .parse()
@@ -60,47 +54,17 @@ function errorCatcher(state: State, undeployed: Piece[], player: Player, availab
 function handleMoveRequest(state: State, undeployed: Piece[], player: Player, available: Move[]) {
     const timer = new Timer()
 
-    // const hasher = (state: State) => {
-    //     let key = ""
-
-    //     enumerateBoard(state.board, field => {
-    //         for (let i = 0; i < field.pieces.length; ++i) {
-    //             const id = (field.position.x << 12) ^ (field.position.y << 8) ^ (field.position.z << 4) ^ (field.pieces[i].type << 3) ^ field.pieces[i].owner
-    //             key += encodeBase64(id)
-    //         }
-    //     })
-
-    //     return key
-    // }
-
-    // console.log(hasher(state), hasher(state))
-
     if (available.length === 0) {
         throw new Error(`No Moves Available`)
     }
 
     if (args.stupid) {
-        console.log("stupid")
         return available[Math.floor(Math.random() * available.length)]
     }
 
     if (available.length < 900) {
-        // const moveMap = available
-        //     .sort(() => Math.random() - 0.5) // shuffle array 
-        //     .map(move => ({ move: move, rating: simulateMove(state, move, next => rate(next, player.color).value) })) // sort array by estimated move order
-
-        // available = moveMap.sort((a, b) => b.rating - a.rating).map(a => a.move)
-
         available = sortMoves(state, available, player.color)
     }
-
-    // enumerateBoard(state.board, field => {
-    //     if (field.pieces.length > 0) {
-    //         console.log(field.position, isDraggable(state, field.position))
-    //     }
-    // })
-
-    //console.log(simulateMove(state, available[0], state => rate(state, player.color)), available[0])
 
     const preRating = handleSpecialCase(state, player, available, undeployed)
     const logic = new NegaScout(state, available, player, 3, 1890 - timer.read())
@@ -113,8 +77,9 @@ function handleMoveRequest(state: State, undeployed: Piece[], player: Player, av
 
     const result = logic.find()
 
-    console.log(timer.read())
-    console.log(result.rating, result.value)
+    console.log(`Finished search after ${timer.read()}ms`)
+    console.log(`Rating: ${result.rating}`)
+    console.log(`Selected move: ${result.value}`)
 
     if (result.success) {
         return result.value!
