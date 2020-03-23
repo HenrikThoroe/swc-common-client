624b26942875fe713a5a86e6f0a6d2cb39bd3ecb
Henrik Thoroe <henrik@thoroee.com>
2020-03-23 13:52:45 +0100

    NPX

--- zip.sh
+++ zip.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-mkdir -p ./archive && rm -f ./archive/player.zip && bestzip ./archive/player.zip ./dist/bundle.js start.sh
+mkdir -p ./archive && rm -f ./archive/player.zip && npx bestzip ./archive/player.zip ./dist/bundle.js start.sh
