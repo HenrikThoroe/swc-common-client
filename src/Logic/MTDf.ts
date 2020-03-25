6e725558e9a42a2cf679bda8b6e248dd3f5a032b
Henrik Thoroe <henrik@thoroee.com>
2020-03-25 20:54:11 +0100

    Setup

---
+++ src/Logic/MTDf.ts
@@ -0,0 +1,10 @@
+import Logic, { SearchResult } from "./Logic";
+import { State } from "@henrikthoroe/swc-client";
+
+export default class MTDf extends Logic {
+
+    find(): SearchResult {
+        throw new Error("Method not implemented.");
+    }
+    
+}
