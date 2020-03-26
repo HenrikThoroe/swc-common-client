f57fc7882fcd35a124040d4256e7d4c2ff060067
Henrik Thoroe <henrik@thoroee.com>
2020-03-26 18:36:14 +0100

    Typo

--- src/Cache/createStateMemoryTable.ts
+++ src/Cache/createStateMemoryTable.ts
@@ -4,7 +4,7 @@ import encodeBase64 from "../utils/encodeBase64";
 export type StateMemoryTable = LookupTable<number, number>
 
 /**
- * Creates a table which holds the rating and of a state with it's turn as key.
+ * Creates a table which holds the rating of a state with it's turn as key.
  */
 export default function createStateMemoryTable(): StateMemoryTable {
     return new LookupTable(70, x => encodeBase64(x))
