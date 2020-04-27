ab6898cee99a85698ea7b250efdf29af831202ec
Henrik <henrik@thoroee.com>
2020-04-27 11:57:33 +0200

    Build

--- webpack.config.js
+++ webpack.config.js
@@ -11,11 +11,9 @@ module.exports = {
             {
                 test: /\.tsx?$/,
                 use: 'ts-loader',
-                // exclude: [
-                //     path.resolve(__dirname, '../performance_tests/')
-                // ],
-                // include: [/node_modules/]
-                // exclude: /node_modules/
+                exclude: [
+                    path.resolve(__dirname, "./exclude.ts")
+                ]
             }
         ]
     },
